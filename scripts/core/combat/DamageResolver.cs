18a2d708331b0efab61f29c8a1356d30878de727
MeowKJ <ijink@icloud.com>
2026-07-24 09:30:03 +0800

    combat: compose element upgrade modifiers

--- scripts/core/combat/DamageResolver.cs
+++ scripts/core/combat/DamageResolver.cs
@@ -9,12 +9,19 @@ public static class DamageResolver
         ArmorTag armorTag,
         float attackerDamageMultiplier = 1f,
         ElementDefenseProfile? targetElementDefense = null,
-        TargetTraitProfile? targetTraits = null)
+        TargetTraitProfile? targetTraits = null,
+        float targetIncomingElementDamageMultiplier = 1f)
     {
         var profileMultiplier = ammo.DamageProfile.Multiplier(weightClass, movementDomain, armorTag);
         var elementMultiplier = DamageElementCatalog.For(ammo.DamageElementId).DamageMultiplier;
         var defenseMultiplier = (targetElementDefense ?? ElementDefenseProfile.Neutral).MultiplierFor(ammo.DamageElementId);
         var counterMultiplier = ammo.CounterRules.MultiplierFor(targetTraits, weightClass, movementDomain, armorTag);
-        return ammo.BaseDamage * profileMultiplier * elementMultiplier * defenseMultiplier * counterMultiplier * attackerDamageMultiplier;
+        return ammo.BaseDamage
+            * profileMultiplier
+            * elementMultiplier
+            * defenseMultiplier
+            * counterMultiplier
+            * attackerDamageMultiplier
+            * targetIncomingElementDamageMultiplier;
     }
 }
